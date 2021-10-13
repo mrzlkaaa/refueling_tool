27abe4ce8c7bcd13fa7e3da0eb5e2c8a819c65b0
Nikita_Ubuntu <nvs38@tpu.ru>
2021-10-13 22:09:20 +0700

    added files to repo, also psql settled up with flask

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
+import os
+from flask import render_template, request
+import numpy as np
+from db import *
 
 
-app = Flask(__name__)
- 
-@app.route("/")
+
+@app.route("/", methods=['GET', 'POST'])
 def hello():
+    if request.method == 'POST':
+        name = request.form['name']
+        oper_name = 'refueling #130'
+        desc = 'refueled two 8-tube FA in cells 7-6, 6-6'
+        date = datetime.now()
+        data = np.array([0,0,0]).tobytes() #* convert to bytes
+        rq = RefuelingDB(name=oper_name, description=desc, date=date, data=data)
+        db.session.add(rq)
+        db.session.commit()
+        print(name)
     return render_template('home.html')
--- db.py
+++ db.py
@@ -2,8 +2,8 @@ import os
 import numpy as np
 from datetime import datetime
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, select, Column, Integer, String, DateTime, LargeBinary
-from sqlalchemy.orm import declarative_base, Session
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 
 load_dotenv()
@@ -12,64 +12,48 @@ user = os.environ['PSQL_USER']
 pwd = os.environ['PSWD']
 port = os.environ['PORT']
 host = os.environ['HOST'] 
-db = os.environ['DB']
+database = os.environ['DB']
 
-engine = create_engine(f'postgresql://{user}:{pwd}@{host}:{port}/{db}', echo=True)
-print(engine.connect())
-Base = declarative_base()
-
-
-def MySession():
-	with Session(engine) as session:
-		return session
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{pwd}@{host}:{port}/{database}'
+db = SQLAlchemy(app)
 
 
-class RefuelingDB(Base):
+class RefuelingDB(db.Model):
 	__tablename__ = 'core_configuration'
-	id = Column(Integer, primary_key=True)
-	refueling_name = Column(String(100))
-	description = Column(String(300))
-	date = Column(DateTime())
-	burnup_data = Column(LargeBinary(length=500))
+	id = db.Column(db.Integer, primary_key=True)
+	refueling_name = db.Column(db.String(100))
+	description = db.Column(db.String(300))
+	date = db.Column(db.DateTime())
+	burnup_data = db.Column(db.LargeBinary(length=500))
 	
 
-	# def __init__(self, name='', description='', date=datetime.now(), data=np.array([0.0, 0.0])):
 	def __init__(self, *args, **kwargs):
 		self.refueling_name = kwargs['name']
 		self.description = kwargs['description']
 		self.date = kwargs['date']
 		self.burnup_data = kwargs['data']
 		# self.burnup_data = bytes(data, 'utf-8')
-		self.session = MySession()
+		# self.session = MySession()
 
 	def __repr__(self):
 		return f'{self.__class__.__name__}(id={self.id}, name={self.refueling_name}, date={self.date}, data={self.burnup_data})'
 
-	def add(self):
-		self.session.add(self)
-		self.session.flush()
-		return self.session.commit()
+
+# def query(): #* query testing 
+# 	session = MySession()
+# 	response = session.query(RefuelingDB.id).all()
+# 	print(response)
+# 	response = session.query(RefuelingDB).filter(RefuelingDB.id == 3).all()
+# 	print(response)
+# 	for i in response:
+# 		configuration = i.burnup_data
+# 		print(i.description)
+# 		arr = np.frombuffer(configuration)
+# 		arr = arr.reshape((6,4))
+# 		print(arr)
+# 	return 
 
 
-def query(): #* query testing 
-	session = MySession()
-	response = session.query(RefuelingDB.id).all()
-	print(response)
-	response = session.query(RefuelingDB).filter(RefuelingDB.id == 3).all()
-	print(response)
-	for i in response:
-		configuration = i.burnup_data
-		print(i.description)
-		arr = np.frombuffer(configuration)
-		arr = arr.reshape((6,4))
-		print(arr)
-	return 
-
-
-if __name__ == '__main__':
-	# test = Refueling('refueling #118', '2 new 6-th tube in cells: 4-6, 6-6', 'here is a data').add()
-	# print(Refueling(name='refueling #118', data=np.array([0,0]), date=datetime.now(), description=''))
-	print(query())
-	# print(select(Refueling))
-	# Base.metadata.create_all(engine)
+# if __name__ == '__main__':
 
